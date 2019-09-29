602f50c7f14896744fbd67dc44005b5e7a02870b
Yulia Pavlova <48909943+Lumia720@users.noreply.github.com>
2019-09-29 14:37:44 +0300

    add generate_tfrecordobject_detection_video.py

---
+++ object_detection_video.py
@@ -0,0 +1,39 @@
+
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Open video file
+video = cv2.VideoCapture("https://www.youtube.com/watch?v=q4UjWTNeTdU")
+
+while(video.isOpened()):
+
+    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    ret, frame = video.read()
+    frame_expanded = np.expand_dims(frame, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.60)
+
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Clean up
+video.release()
+cv2.destroyAllWindows()
